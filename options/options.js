da5db6473b062439973b51451ca0c9216bc5a625
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 08:09:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-wind-dev-57177",
+  url: "https://yellow-wind-dev-57177.botics.co",
+  api: "https://yellow-wind-dev-57177.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
